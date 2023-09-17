b5e3ba6e56f342b3a1d800304353dad8927eb1c4
Ahmet IZGI <dev.ahmetizgi@gmail.com>
2023-09-17 22:11:14 +0300

    chore: unnecessary css removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./Router";
-import "./index.css";
 import "antd/dist/reset.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
